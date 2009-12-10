021efa29f0a752109f33f7a742765c5a7d0c7e15
brian d foy <brian.d.foy@gmail.com>
2009-12-10 16:42:55 -0600

    * Initial creation with dist_cooker

---
+++ lib/Chrome.pm
@@ -0,0 +1,69 @@
+package HTTP::Cookies::Chrome;
+use strict;
+
+use warnings;
+no warnings;
+
+use subs qw();
+use vars qw($VERSION);
+
+$VERSION = '0.10_01';
+
+=head1 NAME
+
+HTTP::Cookies::Chrome - XXX: This is the description
+
+=head1 SYNOPSIS
+
+	use HTTP::Cookies::Chrome;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new
+	{
+	
+	}
+	
+=item init
+
+=cut
+
+sub init
+	{
+	
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/http-cookies-chrome/
+
+=head1 AUTHOR
+
+, C<< <bdfoy@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+
+Copyright (c) 2009, brian d foy, All Rights Reserved.
+
+You may redistribute this under the same terms as Perl itself.
+
+=cut
+
+1;
