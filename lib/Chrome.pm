ffbcfca75cdba3382806282bded9a088036a2893
brian d foy <brian.d.foy@gmail.com>
2009-12-27 22:09:11 +0100

    * Implemented the load() functionality

--- lib/Chrome.pm
+++ lib/Chrome.pm
@@ -1,69 +1,170 @@
+use 5.010;
+use utf8;
+
 package HTTP::Cookies::Chrome;
 use strict;
 
 use warnings;
 no warnings;
 
-use subs qw();
-use vars qw($VERSION);
-
-$VERSION = '0.10_01';
-
 =head1 NAME
 
-HTTP::Cookies::Chrome - XXX: This is the description
+HTTP::Cookies::Chrome - Cookie storage and management for Google Chrome
 
 =head1 SYNOPSIS
 
 	use HTTP::Cookies::Chrome;
 
+	my $cookie_jar = HTTP::Cookies::Chrome->new;
+	$cookie_jar->load( $path_to_cookies );
+
+	# otherwise same as HTTP::Cookies
+
 =head1 DESCRIPTION
 
-=over 4
+This package overrides the C<load()> and C<save()> methods of
+C<HTTP::Cookies> so it can work with Google Chrome cookie files,
+which are SQLite databases.
 
-=item new
+See L<HTTP::Cookies>.
 
-=cut
+=head1 The Chrome cookies table
 
-sub new
-	{
-	
-	}
-	
-=item init
-
-=cut
-
-sub init
-	{
-	
-	}
-
-=back
-
-=head1 TO DO
-
-
-=head1 SEE ALSO
-
+	creation_utc    INTEGER NOT NULL UNIQUE PRIMARY KEY
+	host_key        TEXT NOT NULL
+	name            TEXT NOT NULL
+	value           TEXT NOT NULL
+	path            TEXT NOT NULL
+	expires_utc     INTEGER NOT NULL
+	secure          INTEGER NOT NULL
+	httponly        INTEGER NOT NULL
+	last_access_utc INTEGER NOT NULL
 
 =head1 SOURCE AVAILABILITY
 
-This source is in Github:
+This module is in Github:
 
-	http://github.com/briandfoy/http-cookies-chrome/
+	http://github.com/briandfoy/HTTP-Cookies-Chrome
 
 =head1 AUTHOR
 
-, C<< <bdfoy@cpan.org> >>
+brian d foy, C<< <bdfoy@cpan.org> >>
+
+=head1 CREDITS
+
+Jon Orwant pointed out the problem with dates too far in the future
 
 =head1 COPYRIGHT AND LICENSE
 
+Copyright (c) 2009-2010 brian d foy.  All rights reserved.
 
-Copyright (c) 2009, brian d foy, All Rights Reserved.
-
-You may redistribute this under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
+
+
+use base qw( HTTP::Cookies );
+use vars qw( $VERSION );
+
+use constant TRUE  => 'TRUE';
+use constant FALSE => 'FALSE';
+
+$VERSION = '1.01';
+
+use Data::Dumper;
+use DBI qw(:sql_types);
+
+sub _get_rows
+	{
+	my( $self, $file ) = @_;
+	
+	my $dbh = DBI->connect( "dbi:SQLite:dbname=$file", '', '' );
+	
+	my $sth = $dbh->prepare( 'SELECT * FROM cookies' );
+
+	# Without this, Perl converts the long numbers into
+	# scientific notation
+	foreach my $column ( 1, 6 .. 9 )
+		{
+		$sth->bind_col( $column, undef, SQL_INTEGER );
+		}
+		
+	$sth->execute;
+	
+	my @rows = map { bless $_, 'HTTP::Cookies::Chrome::Record' }
+		@{ $sth->fetchall_arrayref };
+	
+	#print STDERR Dumper( \@rows );
+	
+	\ @rows;
+	}
+	
+sub load
+	{
+    my( $self, $file ) = @_;
+
+    $file ||= $self->{'file'} || return;
+
+# $cookie_jar->set_cookie( $version, $key, $val, $path, 
+# $domain, $port, $path_spec, $secure, $maxage, $discard, \%rest )
+
+ 	foreach my $row ( @{ $self->_get_rows( $file ) } )
+    	{
+		$self->set_cookie(
+			undef, 
+			$row->name,
+			$row->value,
+			$row->path,
+			$row->host_key,
+			undef,
+			undef,
+			$row->secure,
+			$row->expires_utc / 100_000 - gmtime, 
+			0,
+			{}
+			);
+    	}
+
+
+    1;
+	}
+
+sub save
+	{
+    my( $self, $file ) = @_;
+
+    $file ||= $self->{'file'} || return;
+	
+	}
+
+BEGIN {
+package HTTP::Cookies::Chrome::Record;
+use vars qw($AUTOLOAD);
+
+my %columns = map { state $n = 0; $_, $n++ } qw(
+	creation_utc    
+	host_key        
+	name            
+	value           
+	path            
+	expires_utc     
+	secure          
+	httponly        
+	last_access_utc 
+	);
+	
+sub AUTOLOAD
+	{
+	my( $self ) = @_;
+	my $method = $AUTOLOAD;
+	$method =~ s/.*:://;
+	
+	die "" unless exists $columns{$method};
+	
+	$self->[ $columns{$method} ];
+	}
+
+}
 
 1;
--- t/load.t
+++ t/load.t
@@ -1,10 +1,31 @@
-BEGIN {
-	@classes = qw(HTTP::Cookies::Chrome);
-	}
+use Test::More tests => 8;
 
-use Test::More tests => scalar @classes;
+use HTTP::Cookies::Chrome;
+use File::Spec::Functions;
 
-foreach my $class ( @classes )
+my $file = catfile( qw( test-corpus Cookies ) );
+
+my %Domains = qw( 
+	.bing.com      5 
+	www.bing.com   1 
+	.google.fr     2 
+	.www.yahoo.com 1 
+	.yahoo.com     2 
+	.fr.yahoo.com  3 
+	);
+
+my $jar = HTTP::Cookies::Chrome->new( File => $file );
+isa_ok( $jar, 'HTTP::Cookies::Chrome' );
+ok( exists $jar->{COOKIES}, "The COOKIES key is in the jar" );
+
+my $hash = $jar->{COOKIES};
+
+my $domain_count = keys %$hash;
+is( $domain_count, scalar keys %Domains, 'Count of domains is right' );
+
+foreach my $domain ( keys %Domains )
 	{
-	print "Bail out! $class did not compile\n" unless use_ok( $class );
+	my $domain_hash  = $hash->{ $domain }{ '/' };
+	my $count        = keys %$domain_hash;
+	is( $count, $Domains{$domain}, "$domain has $count cookies" ); 	
 	}
