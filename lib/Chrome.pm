b0969b9dabe14ac3a63761c00d9357f78a2d98cc
brian d foy <brian.d.foy@gmail.com>
2009-12-29 15:19:29 +0100

    * basic implementation of save()

--- lib/Chrome.pm
+++ lib/Chrome.pm
@@ -7,6 +7,8 @@ use strict;
 use warnings;
 no warnings;
 
+use POSIX;
+
 =head1 NAME
 
 HTTP::Cookies::Chrome - Cookie storage and management for Google Chrome
@@ -67,19 +69,28 @@ it under the same terms as Perl itself.
 use base qw( HTTP::Cookies );
 use vars qw( $VERSION );
 
-use constant TRUE  => 'TRUE';
-use constant FALSE => 'FALSE';
+use constant TRUE  => 1;
+use constant FALSE => 0;
 
 $VERSION = '1.01';
 
 use Data::Dumper;
 use DBI qw(:sql_types);
 
+sub _dbh { $_[0]->{dbh} }
+
+sub _connect
+	{
+	my( $self, $file ) = @_;
+	my $dbh = DBI->connect( "dbi:SQLite:dbname=$file", '', '' );
+	$_[0]->{dbh} = $dbh;
+	}
+	
 sub _get_rows
 	{
 	my( $self, $file ) = @_;
 	
-	my $dbh = DBI->connect( "dbi:SQLite:dbname=$file", '', '' );
+	my $dbh = $self->_connect( $file );
 	
 	my $sth = $dbh->prepare( 'SELECT * FROM cookies' );
 
@@ -120,22 +131,150 @@ sub load
 			undef,
 			undef,
 			$row->secure,
-			$row->expires_utc / 100_000 - gmtime, 
+			($row->expires_utc / 1_000_000) - gmtime, 
 			0,
 			{}
 			);
     	}
-
 
     1;
 	}
 
 sub save
 	{
-    my( $self, $file ) = @_;
+    my( $self, $new_file ) = @_;
 
-    $file ||= $self->{'file'} || return;
+    $new_file ||= $self->{'file'} || return;
 	
+	my $dbh = $self->_connect( $new_file );	
+	
+	$self->_create_table;
+	$self->_prepare_insert;
+	$self->_filter_cookies;
+	
+	1;
+	}
+
+sub _filter_cookies
+	{
+    my( $self ) = @_;
+
+    $self->scan(
+		sub {
+			my( $version, $key, $val, $path, $domain, $port,
+				$path_spec, $secure, $expires, $discard, $rest ) = @_;
+	
+				return if $discard && not $self->{ignore_discard};
+	
+				return if defined $expires && time > $expires;
+	
+				$expires = do {
+					unless( $expires ) { 0 }
+					else
+						{
+						$expires * 1_000_000
+						}
+					};
+	
+				$secure = $secure ? TRUE : FALSE;
+	
+				my $bool = $domain =~ /^\./ ? TRUE : FALSE;
+	
+				$self->_insert(
+					$domain,
+					$key,
+					$val,
+					$path,
+					$expires,
+					$secure,
+					);
+			}
+		);
+
+	}
+
+sub _create_table
+	{
+	my( $self) = @_;
+
+	$self->_dbh->do( <<'SQL' );
+DROP TABLE cookies
+SQL
+
+	$self->_dbh->do( <<'SQL' );
+CREATE TABLE cookies (
+	creation_utc    INTEGER NOT NULL UNIQUE PRIMARY KEY,
+	host_key        TEXT NOT NULL,
+	name            TEXT NOT NULL,
+	value           TEXT NOT NULL,
+	path            TEXT NOT NULL,
+	expires_utc     INTEGER NOT NULL,
+	secure          INTEGER NOT NULL,
+	httponly        INTEGER NOT NULL,
+	last_access_utc INTEGER NOT NULL
+)
+SQL
+	}
+	
+sub _prepare_insert
+	{
+	my( $self ) = @_;
+	
+	my $sth = $self->{insert_sth} = $self->_dbh->prepare_cached( <<'SQL' );
+INSERT INTO cookies VALUES
+	( 
+	?, 
+	?, ?, ?, ?, 
+	?, 
+	?, 
+	?, 
+	?
+	)
+SQL
+
+	}
+
+{
+my $creation_offset = 0;
+
+sub _insert
+	{
+	my( $self, 					
+		$domain, $key, $value, $path, $expires, $secure, ) = @_;
+		
+	my $sth = $self->{insert_sth};
+	
+	my $creation    = $self->_get_utc_microseconds( $creation_offset++ );
+	print STDERR "creation is [$creation]\n";
+	my $last_access = $self->_get_utc_microseconds;
+	my $httponly    = 0;
+	
+	$sth->bind_param( 1, $creation,    SQL_INTEGER );
+	$sth->bind_param( 6, $expires,     SQL_INTEGER );
+	$sth->bind_param( 7, $secure,      SQL_INTEGER );
+	$sth->bind_param( 8, $httponly,    SQL_INTEGER );
+	$sth->bind_param( 9, $last_access, SQL_INTEGER );
+	
+	$sth->execute(
+		$creation,      # 1
+		$domain,        # 2
+		$key,           # 3
+		$value,         # 4
+		$path,          # 5
+		$expires,       # 6
+		$secure,        # 7
+		$httponly,      # 8
+		$last_access,   # 9
+		);
+
+	}
+}
+
+sub _get_utc_microseconds
+	{	
+	no warnings 'uninitialized';
+	use bignum;
+	POSIX::strftime( '%s', gmtime() ) * 1_000_000 + $_[1];
 	}
 
 BEGIN {
