844bfa3b3149e216f8c00e6f37d18f76a71c5441
brian d foy <brian.d.foy@gmail.com>
2018-06-11 09:10:31 -0400

    Bump copyright year

--- lib/HTTP/Cookies/Chrome.pm
+++ lib/HTTP/Cookies/Chrome.pm
@@ -62,7 +62,7 @@ Jon Orwant pointed out the problem with dates too far in the future
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2009-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2009-2018, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
