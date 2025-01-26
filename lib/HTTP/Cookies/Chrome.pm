5593e323664bb6e65e64098be5d17ecf6abfb553
brian d foy <briandfoy@pobox.com>
2025-01-26 15:51:11 -0500

    Update email address and copyright year

--- lib/HTTP/Cookies/Chrome.pm
+++ lib/HTTP/Cookies/Chrome.pm
@@ -706,7 +706,7 @@ Jon Orwant pointed out the problem with dates too far in the future
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2009-2021, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2009-2025, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the Artistic License 2.0.
